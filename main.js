96791062f151769260ee8114a5b141022cf6d5ae
Pablo <paulvaughan.dev@gmail.com>
2019-02-23 15:27:00 -0500

    commencement of lab day 2

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+// Design guessing game about yourself that involves five YES/NO questions. Be creative and fun.
+let question1 = prompt('What is your real name?')
+let question2 = prompt('Where are you from my friend?')
+// let question3 = prompt('Do you love Basketball?')
+// let question4 = prompt('Are you an aspiring developer?')
+// let question5 = prompt('Is the Lion King your favorite movie?')
+
+if (question1 === 'Pablo'){
+ confirm ('Yessir!')
+}
+
+else {
+  confirm ('Please take a walk!!')
+}
+
+if (question2 === 'Jamaica'){
+  confirm ('Excellent!!')
+}
+
+else {
+  confirm ('No you are an imposter!')
+}
