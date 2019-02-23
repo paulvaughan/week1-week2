64d0e87476b0f6b9f71af889212cbf9952eab8f7
Pablo <paulvaughan.dev@gmail.com>
2019-02-23 16:41:50 -0500

    updated changes to js code syntax

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 'use strict'
 // Design guessing game about yourself that involves five YES/NO questions. Be creative and fun.
-let question1 = prompt('What is your real name?')
-let question2 = prompt('Where are you from my friend?')
-// let question3 = prompt('Do you love Basketball?')
-// let question4 = prompt('Are you an aspiring developer?')
-// let question5 = prompt('Is the Lion King your favorite movie?')
-
+let question1 = prompt('What is your real name?');
 if (question1 === 'Pablo'){
- confirm ('Yessir!')
-}
+  confirm ('Yessir!')
+ }
+ 
+ else {
+   confirm ('Please take a walk!!')
+ }
 
-else {
-  confirm ('Please take a walk!!')
-}
-
+let question2 = prompt('Where are you from my friend?');
 if (question2 === 'Jamaica'){
   confirm ('Excellent!!')
 }
@@ -21,3 +17,9 @@ if (question2 === 'Jamaica'){
 else {
   confirm ('No you are an imposter!')
 }
+// let question3 = prompt('Do you love Basketball?')
+// let question4 = prompt('Are you an aspiring developer?')
+// let question5 = prompt('Is the Lion King your favorite movie?')
+
+
+
